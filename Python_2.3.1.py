2a2c421398144714da95bc95a136281ce2cce0be
Arsentij1986 <108745608+Arsentij1986@users.noreply.github.com>
2025-07-02 01:28:22 +0300

    Update and rename Python_2.3.1 to Python_2.3.1.py

---
+++ Python_2.3.1.py
@@ -0,0 +1,4 @@
+pitomec=str(input("Напиши вид питомца"))
+vozrast=str(input("Введи его возраст"))
+klichka=str(input("Введи имя питомца"))
+print(f"Это {pitomec} по кличке {klichka}. Возраст: {vozrast}")
