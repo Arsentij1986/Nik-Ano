5c3db2e9332c2a231a82ff82a3512c9e0a4a9374
Arsentij1986 <108745608+Arsentij1986@users.noreply.github.com>
2025-07-02 04:24:34 +0300

    Create Python_2.5.3.py

---
+++ Python_2.5.3.py
@@ -0,0 +1,13 @@
+mike = int(input("Сколько долларов у Майкла? "))
+ivan = int(input("Сколько долларов у Ивана? "))
+x = int(input("Какая минимальная сумма для инвестиций? "))
+if (x <= mike) and (x <= ivan):
+	print("2")
+elif (x <= mike) and (x > ivan):
+	print("Mike")
+elif (x <= ivan) and (x > mike):
+	print("Ivan")
+elif (x > mike) and (x > ivan) and (x <= (mike + ivan)):
+	print("1")
+else:
+  print("0")
